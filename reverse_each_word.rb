69de5c38d451abdeda8cfbebd22851f9d120dab9
Brandon Weaver <a7xncob@gmail.com>
2019-11-06 02:49:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   temp = string.split(' ')
   string = ""
   temp.collect do |word|
-    string << word != temp.last ? word.reverse : "#{word.reverse} "
+    string << word == temp.last ? word.reverse : "#{word.reverse} "
   end
   return string
 end
